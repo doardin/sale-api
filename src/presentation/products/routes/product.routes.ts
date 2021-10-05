383e775e9bad2abe736667b88850c9170ba107eb
Eduardo Lopes <duh.lopes234@hotmail.com>
2021-10-05 20:43:47 -0300

    Products routes and celebrate validation added

--- src/presentation/products/routes/product.routes.ts
+++ src/presentation/products/routes/product.routes.ts
@@ -1,17 +1,39 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
 import ProductController from "../controller/ProductController";
 
 const productRouters = Router();
 const productControler = new ProductController();
 
-productRouters.post("/", productControler.save);
+productRouters.post("/", celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required() 
+    }
+}), productControler.save);
 
-productRouters.put("/", productControler.update);
+productRouters.put("/", celebrate({
+    [Segments.BODY]: {
+        id: Joi.string().uuid().required(),
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required()
+    }
+}), productControler.update);
 
 productRouters.get("/", productControler.listProducts);
 
-productRouters.get("/:id", productControler.findProductById);
+productRouters.get("/:id", celebrate({
+    [Segments.PARAMS]: {
+        id: Joi.string().uuid().required()
+    }
+}) , productControler.findProductById);
 
-productRouters.delete("/:id", productControler.delete);
+productRouters.delete("/:id", celebrate({
+    [Segments.PARAMS]: {
+        id: Joi.string().uuid().required()
+    }
+}), productControler.delete);
 
 export default productRouters;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import { errors } from "celebrate";
 import cors from "cors";
 import routes from "./routes"
 import '@shared/typeorm';
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
 app.use((error : Error, request : Request, response : Response, next : NextFunction) => {
     if(error instanceof CustomException){
         return response.status(error.statusCode).json({
